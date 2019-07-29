0f2bb18a0b3cc271f66d9949b600710b9aeb98bc
xwan <xwan@redhat.com>
2019-07-29 11:36:59 +0800

    file clean up

--- setup.sh
+++
@@ -1,3 +0,0 @@
-sudo mkdir ./libgit2/build/ && cd ./libgit2/build
-sudo cmake ./libgit2/ -d ./libgit2/build/
-sudo cmake ./libgit2/build/ --build -d ./libgit2/build/
--- test.cpp
+++ test.cpp
@@ -2,10 +2,11 @@
 #include <iostream>
 #include <string>
 #define INFTY = 65535
+#define ELMIMINATED = -1
 using namespace std;
 
 /* Improvement that can be made:
- * 1. Look-back methods are used in parser. This can be 
+ * 1. Look-back methods are used in Parserr. This can be 
  * changed to look-forward method
  * 2. Nothing is done until a line of statement is made.
  * This can be improved by directly locate the Enter 
@@ -14,8 +15,6 @@ using namespace std;
 
 const int identifier_size = 11;
 
-Patch patch;
-
 string identifiers[identifier_size] = {
     "+", "-", "diff --git",
     "+++", "---", "new file mode", 
@@ -23,18 +22,51 @@ string identifiers[identifier_size] = {
     "\\No newline at the end of file", "index"
 };
 
-void deal_with_one_line(int ident_id, char * st){
+
+class Parser {
+private:
+    Patch patch;
+    FileChange * cur_file;
+    int cur_ori_num;
+    int cur_fin_num;
+    string commit_hash;
+    void deal_with_one_line(int ident_id, char * st);
+    bool hit_identifier(char * op_buffer, int & ident_id);
+    string Parser_patch_git_info(string str);
+public:
+};
+
+Parser::Parser(string commit_hash) {
+    this -> cur_ori_num = 0;
+    this -> cur_fin_num = 0;
+    this -> commit_hash = commit_hash;
+}
+
+void Parser::deal_with_one_line(int ident_id, char * st){
     switch (ident_id){
-        case 0:
-            
+        case 0:         // +
+            LineChange l = {this->cur_file->orig_dir, this->cur_file->final_dir, 
+                        cur_ori_num, cur_fin_num, (string)st, this->commit_hash};
+            (*(patch.file_changes.end())).linechanges.push_back(l);
+            break;
+        case 1:         // -  
             break;
         
+        case 3:         // +++
+            break;
+        case 4:         // ---
+            break;
+        case 6:         // normal lines
+            break;
+        case 7:         // @@
+
+            break;
         default:
             break;
     }
 }
 
-bool hit_identifier(char * op_buffer, int & ident_id) {
+bool Parser::hit_identifier(char * op_buffer, int & ident_id) {
     for (int i = 0; i < identifier_size; i++) {
         if (identifiers[i].compare(op_buffer)) {
             ident_id = i;
@@ -45,13 +77,13 @@ bool hit_identifier(char * op_buffer, int & ident_id) {
     return false;
 }
 
-string parse_patch_git_info(string str) {
+string Parser::Parser_patch_git_info(string str) {
     char * op_buffer = new char[32];    // buffer for operator
     int ident_id = INFTY;
     char * st_buffer = new char[128];   // buffer for statement
     int op_buf_size = 0;
     int st_buf_size = 0;
-    int ptr = 0
+    int ptr = 0;
     for(int i = 0; i < str.length(); i++) {
         if (str[i] == '\n') {
             deal_with_one_line(ident_id, st_buffer);
@@ -78,6 +110,7 @@ string parse_patch_git_info(string str) {
         }
     }
 }
+
 
 
 int main(int argc, char const *argv[]) {
