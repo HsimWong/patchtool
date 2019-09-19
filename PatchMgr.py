e4a9d3181ade04a94da318c40e83a6dac2210c30
HsimWong <16074101@emails.bjut.edu.cn>
2019-09-19 10:12:55 +0800

    clean source code, delete comments for debugging

--- PatchMgr.py
+++ PatchMgr.py
@@ -1,5 +1,4 @@
 import re
-import Fake
 
 '''
 fc = {
@@ -17,7 +16,6 @@ line = {
 }
 '''
 
-
 class PatchMgr(object):
     def __init__(self, git_body):
         self.git_body = git_body
@@ -26,41 +24,22 @@ class PatchMgr(object):
 
     def parse(self):
         file_git_array = re.split(r'diff --git ', self.git_body)
-        # print(len(file_git_array))
         for file_git in file_git_array:
             if len(file_git) < 10:
                 continue
             self.file_changes.append(self.make_file(file_git))
-        # print(self.file_changes)
 
     def make_modi(self, modi_git):
-        # print(modi_git)
         raw_lines = modi_git.split('\n')
-        # print(raw_lines[0])
-        # print(lines[1])
-        # print(lines[2])
-
         int_raw = re.findall('[0-9]*', raw_lines[0])
-        # print(raw_lines[0])
-        # print(int_raw)
-        # print(sum([int(x) for x in int_raw]))
-        # if int_raw[1] == "":
-        #     ori_start = int(int_raw[1])
-        #     ori_offst = int(int_raw[3])
-        #     new_start = int(int_raw[6])
-        #     new_offst = int(int_raw[8])
-        # else:  
         ori_start = int(int_raw[1])
         ori_offst = int(int_raw[3])
         new_start = int(int_raw[6])
         new_offst = int(int_raw[8])
-
-        # print(ori_start, ori_offst, new_start, new_offst)
         lines = []
         cur_off_old = 0
         cur_off_new = 0
         for raw_line in raw_lines[1:]:
-            # print(line)
             typ = ""
             if len(raw_line) == 0:
                 continue
@@ -80,9 +59,6 @@ class PatchMgr(object):
                    "new_line_num": new_start + cur_off_new,
                    "type": typ, "content": raw_line}
             lines.append(apd)
-            # print(apd)
-        # print("yes")
-
         return {
             "ori_start": ori_start, "ori_offset": ori_offst,
             "new_start": new_start, "new_offset": new_offst,
@@ -95,45 +71,17 @@ class PatchMgr(object):
         split_lines = file_git.split('\n')
         for line in file_git.split('\n')[0:4]:
             file_head += line + '\n'
-        # print(split_lines[0])
-
         ori_dir = split_lines[0][2:split_lines[0].find(" b/")]
-        # print(ori_dir)
         new_dir = split_lines[0][split_lines[0].find(" b/") + 3:]
-
         file_body = file_git[file_git.find("@@") + 3:]
-
         modi_gits = file_body.split("\n@@ ")
-        # print(len(modi_gits))
-        # print((modi_gits[0]))
         modifs = []
         if not (len(split_lines) < 5):
-            # print(split_lines[5])
-            # print(modi_gits)
             try:
                 for modi in modi_gits:
-                    # print(modi)
-                    # print("_________________________________")
-                    # print(modi)
-                    # if len(modi.split('\n')) <= 4:
-                    #     break
                     modifs.append(self.make_modi(modi))
             except ValueError:
                 pass
 
         return {"ori_dir": ori_dir, "new_dir": new_dir,
                 "modifs": modifs, "head_str": file_head}
-
-
-if __name__ == "__main__":
-    file_git = ""
-    add_git = ""
-    with open("./sample_new.txt") as f:
-        add_git = f.read()
-    with open("./sample_bug.txt") as f:
-        file_git = f.read()
-
-    a = PatchMgr(file_git)
-    b = PatchMgr(add_git)
-    out_str = (Fake.getFakeInfo(a, b))
-    print(out_str)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import PatchMgr
-import Fake 
+import Fake
 import argparse
 import os
 import re
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -11,24 +10,15 @@ if __name__ == '__main__':
                         help='The directory where this patch tool is hoped to be used')
     parser.add_argument('-p', type=str, required=True,
                         help="Directory of the patch file")
-
     args = parser.parse_args()
     pat_dir = str(args.p)
     git_repo = str(args.d)
-    # pat_dir = "./patch-list1"
-    # git_repo = "/home/xwan/debug/libvirt-rhel"
     old_patch_str = ""
     with open(str(pat_dir), "r") as f:
         old_patch_str = f.read()
-    # print("Start parsing each commit......")
     patches_array = re.split(r"-- \n[0-9,\.]*\n*", old_patch_str)
-
-    # for raw_patch in patches_array:
     file_count = 0
-    # raw_patch = patches_array[0]
     for raw_patch in patches_array:
-        # print(len(raw_patch))
-        # print("Working on file %d"%file_count)
         if len(raw_patch) == 0:
             break 
         file_count += 1
@@ -37,16 +27,10 @@ if __name__ == '__main__':
         patch_head = raw_patch[:patch_head_ind]
         patch_body = raw_patch[patch_head_ind+2:]
         patch_hash = patch_head[5:20]
-        # print("Got the commit hash......")
         git_info_now = os.popen("cd %s && git diff %s"%
                                 (git_repo, patch_hash)).read()
-        # print("Got git-diff info from the commit to now version......")
-        # print(git_info_now[0:100])
         mgr_bug = PatchMgr.PatchMgr(patch_body)
-        # print("Generated bug patch mgr")
         mgr_now = PatchMgr.PatchMgr(git_info_now)
-        # print("Generated now patch mgr")
         out_str = patch_head + '\nd' + Fake.getFakeInfo(mgr_bug, mgr_now)[12:]
-        
         print(out_str)
 
