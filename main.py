7aea617a9ec8d2512494c6c282072ccaac40bfcc
HsimWong <16074101@emails.bjut.edu.cn>
2019-09-25 18:44:08 +0800

    fix plural condition

--- main.py
+++ main.py
@@ -5,25 +5,31 @@ import os
 import re
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-d', type=str, required=True,
-                        help='The directory where this patch tool is hoped to be used')
-    parser.add_argument('-p', type=str, required=True,
-                        help="Directory of the patch file")
-    args = parser.parse_args()
-    pat_dir = str(args.p)
-    git_repo = str(args.d)
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-d', type=str, required=True,
+    #                     help='The directory where this patch tool is hoped to be used')
+    # parser.add_argument('-p', type=str, required=True,
+    #                     help="Directory of the patch file")
+    # args = parser.parse_args()
+    # pat_dir = str(args.p)
+    # git_repo = str(args.d)
+    # old_patch_str = ""
+    # with open(str(pat_dir), "r") as f:
+    #     old_patch_str = f.read()
+    pat_dir = '/home/xwan/debug/total.patch'
+    git_repo = '/home/xwan/debug/libvirt-rhel'
     old_patch_str = ""
-    with open(str(pat_dir), "r") as f:
+    with open(str(pat_dir), 'r') as f:
         old_patch_str = f.read()
+
     patches_array = re.split(r"-- \n[0-9,\.]*\n*", old_patch_str)
     file_count = 0
     for raw_patch in patches_array:
         if len(raw_patch) == 0:
             break 
         file_count += 1
-        patch_head_ind = re.search(" [0-9]* file(s)? changed, [0-9]* (insertions\(\+\)|deletions\(\-\))"+\
-                "(, [0-9]* deletions\(\-\))?\n?( [a-z]{1,} mode [0-9]* [0-9,A-Z,a-z,\/,\.,\-]*\n)*", raw_patch).span()[1]
+        patch_head_ind = re.search(" [0-9]* file(s)? changed, [0-9]* (insertion(s)?\(\+\)|deletion(s)?\(\-\))"+\
+                "(, [0-9]* deletion(s)?\(\-\))?\n?( [a-z]{1,} mode [0-9]* [0-9,A-Z,a-z,\/,\.,\-]*\n)*", raw_patch).span()[1]
         patch_head = raw_patch[:patch_head_ind]
         patch_body = raw_patch[patch_head_ind+2:]
         patch_hash = patch_head[5:20]
