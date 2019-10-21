f4008c9fe0bebf5efea05200139bf7112893aa0c
HsimWong <xwan@redhat.com>
2019-10-21 10:12:34 -0400

    changed strategy: only make diff file with the files that are changed, rather than diff the whole git repo. The tool is now ready to use

--- main.py
+++ main.py
@@ -32,9 +32,16 @@ def main():
         patch_head = raw_patch[:patch_head_ind]
         patch_body = raw_patch[patch_head_ind+2:]
         patch_hash = patch_head[5:20]
-        git_info_now = os.popen("cd %s && git diff %s"%
-                                (git_repo, patch_hash)).read()
         mgr_bug = PatchMgr.PatchMgr(patch_body)
+        # mgr_bug.file_changes[0]
+        file_name_list = [file_change['new_dir'] for file_change in mgr_bug.file_changes]
+        cmd_get_git = "cd %s && git diff %s "%(git_repo, patch_hash)
+        for file_name in file_name_list:
+            cmd_get_git += file_name + ' '
+        print(cmd_get_git)
+        git_info_now = os.popen(cmd_get_git).read()
+        # print(git_info_now)
+        # mgr_bug = PatchMgr.PatchMgr(patch_body)
         mgr_now = PatchMgr.PatchMgr(git_info_now)
         out_str = patch_head + '\nd' + Fake.getFakeInfo(mgr_bug, mgr_now)[12:]
         print(out_str)
