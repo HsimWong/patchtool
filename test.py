120b4035e4fe87004ab583657587c062e1520ae1
xwan <xwan@redhat.com>
2019-11-21 23:47:55 +0800

    fixed cases where files are deleted in the tested commit

--- test.py
+++ test.py
@@ -3,10 +3,17 @@ import PatchMgr
 def testify(git_repo, out_str, commit_hash):
     status = True
     file_mgr = PatchMgr.PatchMgr(out_str).file_changes[0]
-    
+    verify_str = ""
+
+    if file_mgr['new_dir'] == "":
+        status = False
+        verify_str += "Commit %s: file %s was deleted in this commit.\n" \
+                 % (commit_hash, file_mgr['ori_dir'])
+        return verify_str, status
+
     with open(git_repo + '/' + file_mgr['new_dir']) as f:
         file_text = f.readlines()
-    verify_str = ""
+
     for modif in file_mgr['modifs']:
         check_line = modif['lines'][0]
         check_num = modif['new_start']
@@ -17,4 +24,4 @@ def testify(git_repo, out_str, commit_hash):
             verify_str += "Commit %s: Line %d at file %s might be incorrect.\n" \
                 % (commit_hash, check_num, file_mgr['new_dir'])
     return verify_str, status
-    
+
